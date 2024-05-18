56ca5338e9e59610d64dd9249c0d337de1c455de
Ashvpatel <ashvpatel@algomau.ca>
2024-05-18 02:55:05 +0000

    added failed attempt session variable and redirect it to index.php

--- validate.php
+++ validate.php
@@ -1,14 +1,24 @@
 <?php
 
+  session_start();
+
   $valid_username = "mike";
   $valid_password = "password";
 
   $username = $_REQUEST["username"];
+  $_SESSION["username"] = $username;
   $password = $_REQUEST["password"];
 
   if ($valid_username == $username && $valid_password == $password){
-    echo "success";
-  } else{
-    echo "failure";
+    header( "Location: /index.php" );
+  } else {
+    
+    if(!isset($_SESSION["failed_attempts"])){
+      $_SESSION["failed_attempts"] = 1;
+    } else{
+      $_SESSION["failed_attempts"] = $_SESSION["failed_attempts"]+1; 
+    }
+
+    echo "This is unsuccessful attempt number: " . $_SESSION["failed_attempts"];
   } 
 ?>
